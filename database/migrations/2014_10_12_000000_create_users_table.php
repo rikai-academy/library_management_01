658282846938f90e603be7b765a6013b20f0789a
LearnRikai <levii.reys@gmail.com>
2021-07-28 20:38:15 +0700

    login-logout-sigin

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,11 +17,11 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->string('address');
-            $table->string('image_user');
+            $table->string('address')->nullable();
+            $table->string('image_user')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('role_id');
+            $table->string('role_id')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
