abb1fd63fe6cb00c148f4913aa613bfa0f0766b4
LearnRikai <levii.reys@gmail.com>
2021-07-28 20:38:15 +0700

    Create View Home Page Admin

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,9 +13,9 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/', function () {
-    return view('welcome');
-});
+
+
+Route::get('/admin', [AdminController::class, 'home'])->name('admin.home');
 
 Route::resources([
     'author' => 'AuthorController',
